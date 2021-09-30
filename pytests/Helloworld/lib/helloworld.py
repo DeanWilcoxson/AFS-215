0b69629420d86433d250149a8504da9d40029fc6
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2021-09-29 19:14:57 -0700

    9/29/21

---
+++ pytests/Helloworld/lib/helloworld.py
@@ -0,0 +1,2 @@
+def Hello(name: str) -> str:
+    return f'Hello world my name is {name}'
---
+++ pytests/Helloworld/tests/test_helloworld.py
@@ -0,0 +1,8 @@
+import pytest
+from lib.helloworld import Hello
+
+
+# class test_helloworld:
+def test_helloworld():
+    hello = Hello("Dean")
+    assert hello == "Hello world my name is Dean"
