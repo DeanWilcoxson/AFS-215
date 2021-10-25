0bb1aea61557c54393bd025f9d92e6017d08f798
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2021-10-25 06:21:16 -0700

    10/25/21

--- week5/Node_FizzBuzz_Kata/test/fizzbuzz_kata_test.js
+++ week5/Node_FizzBuzz_Kata/test/fizzbuzz_kata_test.js
@@ -5,11 +5,9 @@ describe('FizzBuzz', ()=>{
     it('should return FizzBuzz', () => {
         assert.equal(fizzbuzz(15), 'FizzBuzz')
     });
-
     it('should return 1', () => {
         assert.equal(fizzbuzz(1), 1)
     });
-
     it('should return 2', () => {
         assert.equal(fizzbuzz(2), 2)
     });
